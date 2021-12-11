33c6e669cd8d638469218836813734c8c87b220e
PoutsBF <stephane.lepoutere@free.fr>
2021-12-11 14:59:46 +0100

    préparation...

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+SECRET_KEY = "I+z9K>6H'g\\p>R?^W'j44r{S"
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+
 from fbapp import app
 
 if __name__ == "__main__":
